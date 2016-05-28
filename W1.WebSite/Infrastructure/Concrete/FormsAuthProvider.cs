b6465c400e15bb9ec85fa28c6ea711e70ed5bf77
Kindergir <gir.sipovski@gmail.com>
2016-05-29 00:04:30 +0500

    5

---
+++ W1.WebSite/Infrastructure/Concrete/FormsAuthProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using W1.WebUI.Infrastructure.Abstract;
+using System.Web.Security;
+
+namespace W1.WebUI.Infrastructure.Concrete
+{
+    public class FormsAuthProvider : IAuthProvider
+    {
+        public bool Authenticate(string username, string password)
+        {
+            bool result = FormsAuthentication.Authenticate(username, password);
+            if (result)
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+            }
+            return result;
+        } 
+    }
+}
