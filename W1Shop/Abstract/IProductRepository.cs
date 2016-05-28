4e4685f0d3a972e394380b5af2e86bb5ed01dea2
Kindergir <gir.sipovski@gmail.com>
2016-05-28 21:17:06 +0500

    Ollo

--- W1Shop/Abstract/IProductRepository.cs
+++ W1Shop/Abstract/IProductRepository.cs
@@ -7,5 +7,7 @@ namespace W1.Domain.Abstract
     public interface IProductRepository
     {
         IQueryable<Product> Products { get; }
+        void SaveProduct(Product product);
+        Product DeleteProduct(int productID); 
     }
 }
